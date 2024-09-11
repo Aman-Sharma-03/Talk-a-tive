1dd1e5b29372b6dfa0af443e1ff4b9808bb6318c
JAI <jai.sharma2023a@gmail.com>
2024-09-12 01:19:59 +0530

    cleaning chatpage.js

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -2,16 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const ChatPage = () => {
-  const [chats, setChats] = useState([]);
-
-  // const fetchChats = async () =>{
-  //   const data = await axios.get('/api/chat');
-  //   setChats(data.data);
-  // }
-  useEffect(() => {
-    // fetchChats();
-  }, []);
-  if(!chats)  return (<div>No chats</div>)
+  
   return (
     <div>
       {
